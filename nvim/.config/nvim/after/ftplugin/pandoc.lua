c7892f3fe87f2a48cc3bbe1538e1d38da467c5ff
saher <msaher.shair@gmail.com>
2023-09-01 22:27:32 +0300

    more changes

--- nvim/.config/nvim/after/ftplugin/pandoc.lua
+++ nvim/.config/nvim/after/ftplugin/pandoc.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<tab>', "za", { buffer = true })
 
-vim.keymap.set('n', 'A', "g$a", { buffer = true })
-vim.keymap.set('n', 'I', "g^a", { buffer = true })
+vim.keymap.set('n', 'A', "g$i", { buffer = true })
+vim.keymap.set('n', 'I', "g^i", { buffer = true })
 
 vim.opt_local.formatprg = vim.g['pandoc#formatting#equalprg']
