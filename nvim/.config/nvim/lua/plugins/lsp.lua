a59b6f393dc5917829c01799b8942c1716aea8ac
saher <msaher.shair@gmail.com>
2024-01-19 18:53:24 +0300

    lsp improvements

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,52 +1,59 @@
+local function map(modes, lhs, rhs, opts, desc)
+    local options = vim.tbl_extend('force', opts, {desc = desc})
+    vim.keymap.set(modes, lhs, rhs, options)
+end
+
+local function on_attach(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    map("n", "gD", vim.lsp.buf.declaration, bufopts, "declaration")
+    map("n", "gd", vim.lsp.buf.definition, bufopts, "definition")
+    map("n", "K", vim.lsp.buf.hover, bufopts, "hover")
+    map("n", "gI", vim.lsp.buf.implementation, bufopts, "implementation")
+    map("n", "g<C-k>", vim.lsp.buf.signature_help, bufopts, "signature_help")
+    map("n", "gr", vim.lsp.buf.references, bufopts, "references")
+
+    map("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, bufopts, "workspace add")
+    map("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, bufopts, "remove workspace")
+    map("n", "<leader>lwl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts, "list workspaces")
+
+    map("n", "<leader>ld", vim.lsp.buf.type_definition, bufopts, "type_definition")
+    map("n", "<leader>lrn", vim.lsp.buf.rename, bufopts, "rename")
+    map("n", "<leader>la", vim.lsp.buf.code_action, bufopts, "code_action")
+    map("n", "<leader>lf", function()
+        vim.lsp.buf.format({ async = true })
+    end, bufopts, "format")
+end
+
 local config = function()
 	local lsp = require("lspconfig")
 
 	-- Mappings.
 	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-	local opts = { noremap = true, silent = true }
-	vim.keymap.set("n", "<leader><leader>ge", vim.diagnostic.open_float, opts)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+	local map_opts = { noremap = true, silent = true }
+	map("n", "[d", vim.diagnostic.goto_prev, map_opts)
+	map("n", "]d", vim.diagnostic.goto_next, map_opts)
+	map("n", "<leader><leader>df", vim.diagnostic.open_float, map_opts)
+	map("n", "<leader><leader>dq", vim.diagnostic.setloclist, map_opts)
 
 	-- Use an on_attach function to only map the following keys
 	-- after the language server attaches to the current buffer
-	local on_attach = function(client, bufnr)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-		-- Mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local bufopts = { noremap = true, silent = true, buffer = bufnr }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-		vim.keymap.set("n", "gI", vim.lsp.buf.implementation, bufopts)
-		vim.keymap.set("n", "g<C-k>", vim.lsp.buf.signature_help, bufopts)
-		vim.keymap.set("n", "<leader>Wa", vim.lsp.buf.add_workspace_folder, bufopts)
-		vim.keymap.set("n", "<leader>Wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-		vim.keymap.set("n", "<leader>Wl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, bufopts)
-		vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-		vim.keymap.set("n", "<leader><leader>a", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-		vim.keymap.set("n", "<leader><leader>gf", function()
-			vim.lsp.buf.format({ async = true })
-		end, bufopts)
-	end
-
 	-- cmp
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	lsp.clangd.setup({ on_attach = on_attach, capabilities = capabilities })
-	lsp.pyright.setup({ on_attach = on_attach, capabilities = capabilities })
-	lsp.cssls.setup({ on_attach = on_attach, capabilities = capabilities })
-	lsp.texlab.setup({ on_attach = on_attach, capabilities = capabilities })
-	lsp.tsserver.setup({ on_attach = on_attach, capabilities = capabilities })
-
-	lsp.rust_analyzer.setup({ on_attach = on_attach, capabilities = capabilities })
+    local lsp_opts = {on_attach = on_attach, capabilities = capabilities}
+	lsp.clangd.setup(lsp_opts)
+	lsp.pyright.setup(lsp_opts)
+	lsp.cssls.setup(lsp_opts)
+	lsp.texlab.setup(lsp_opts)
+	lsp.tsserver.setup(lsp_opts)
+	lsp.rust_analyzer.setup(lsp_opts)
 
 	lsp.lua_ls.setup({
 		on_attach = on_attach,
